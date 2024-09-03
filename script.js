a8850fe3c8ec913895d74dc5f4c76effb6e82d9f
aastha231074 <asingla1101@gmail.com>
2024-09-03 17:16:31 +0530

    changing the displayed dice image to the random dice number generated image

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+const dice = document.querySelector('.btn--roll');
+dice.addEventListener('click', function () {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  let imageElement = document.querySelector('.dice');
+  imageElement.src = `dice-${randomNumber}.png`;
+});
